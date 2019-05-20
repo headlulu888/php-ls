a084e96b241b72cfb095d72b536f9642a9927e28
headlulu888 <headlulu888@gmail.com>
2019-05-20 22:26:30 +0300

    work6

--- index.php
+++ index.php
@@ -1,34 +1,21 @@
 <?php
 
-$bmw = [
-    "model" => "X5",
-    "speed" => 120, 
-    "doors" => 5,
-    "year" => "2015"
-];
+echo "<table>";
 
-$toyota = [
-    "model" => "Camry",
-    "speed" => 200,
-    "doors" => 3,
-    "year" => "2010"
-];
+for($i = 1; $i <= 10; $i++) {
+    echo "<tr>";
 
-$opel = [
-    "model" => "Astra",
-    "speed" => 100,
-    "doors" => 3,
-    "year" => "2007"
-];
+    for($j = 1; $j <= 10; $j++) {
+        if ($i % 2 == 0 && $j % 2 == 2) {
+            echo "<td>$j * $i = (" . $i * $j . ")</td>";
+        } else if ($i % 2 !== 0 && $j % 2 !== 2) {
+            echo "<td>$j * $i = [" . $i * $j . "]</td>";
+        } else {
+            echo "<td>$j * $i = " . $i * $j . "</td>";
+        }
+    }
 
-$cars = [];
+    echo "</tr>";
+}
 
-array_push($cars, $bmw, $toyota, $opel);
-
-echo "<pre>";
-print_r($cars);
-echo "</pre>";
-
-echo "Car bmw <br>" . $cars[0]['model'] . " " . $cars[0]['speed'] . " " . $cars[0]['doors'] . " " . $cars[0]['year'] . "<br>" . "<br>";
-echo "Car toyota <br>" . $cars[1]['model'] . " " . $cars[1]['speed'] . " " . $cars[1]['doors'] . " " . $cars[1]['year'] . "<br>" . "<br>";
-echo "Car opel <br>" . $cars[1]['model'] . " " . $cars[1]['speed'] . " " . $cars[1]['doors'] . " " . $cars[1]['year'];
+echo "</table>";
