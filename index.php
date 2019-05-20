5814d4d191c6f4f487bbe074e031c973c8a57b1c
headlulu888 <headlulu888@gmail.com>
2019-05-20 22:16:06 +0300

    work5

--- index.php
+++ index.php
@@ -1,19 +1,34 @@
 <?php
 
-$day = date('6');
+$bmw = [
+    "model" => "X5",
+    "speed" => 120, 
+    "doors" => 5,
+    "year" => "2015"
+];
 
-switch ($day) {
-    case 1:
-    case 2:
-    case 3:
-    case 4:
-    case 5:
-        echo "Это рабочий день”";
-        break;
-    case 6:
-    case 6:
-        echo "Это выходной день";
-        break;
-    default:
-        echo "Неизвестный день";
-}
+$toyota = [
+    "model" => "Camry",
+    "speed" => 200,
+    "doors" => 3,
+    "year" => "2010"
+];
+
+$opel = [
+    "model" => "Astra",
+    "speed" => 100,
+    "doors" => 3,
+    "year" => "2007"
+];
+
+$cars = [];
+
+array_push($cars, $bmw, $toyota, $opel);
+
+echo "<pre>";
+print_r($cars);
+echo "</pre>";
+
+echo "Car bmw <br>" . $cars[0]['model'] . " " . $cars[0]['speed'] . " " . $cars[0]['doors'] . " " . $cars[0]['year'] . "<br>" . "<br>";
+echo "Car toyota <br>" . $cars[1]['model'] . " " . $cars[1]['speed'] . " " . $cars[1]['doors'] . " " . $cars[1]['year'] . "<br>" . "<br>";
+echo "Car opel <br>" . $cars[1]['model'] . " " . $cars[1]['speed'] . " " . $cars[1]['doors'] . " " . $cars[1]['year'];
