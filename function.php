e66b6a977275ac3b3e632dd2f619c8522d3db4ed
headlulu888 <headlulu888@gmail.com>
2019-05-21 16:21:09 +0300

    work1

---
+++ function.php
@@ -0,0 +1,20 @@
+<?php
+
+// Задача 1
+function task1($array, $clue = false) {
+    if ($clue == true) {
+        $str = "";
+
+        foreach($array as $arr) {
+            $str .= $arr;
+        }
+
+        return $str;
+    } else {
+        foreach($array as $arr) {
+            echo "<p>$arr</p>";
+        }
+    }
+}
+
+?>
--- index.php
+++ index.php
@@ -1,21 +1,7 @@
 <?php
 
-echo "<table>";
+require_once "./function.php";
 
-for($i = 1; $i <= 10; $i++) {
-    echo "<tr>";
-
-    for($j = 1; $j <= 10; $j++) {
-        if ($i % 2 == 0 && $j % 2 == 2) {
-            echo "<td>$j * $i = (" . $i * $j . ")</td>";
-        } else if ($i % 2 !== 0 && $j % 2 !== 2) {
-            echo "<td>$j * $i = [" . $i * $j . "]</td>";
-        } else {
-            echo "<td>$j * $i = " . $i * $j . "</td>";
-        }
-    }
-
-    echo "</tr>";
-}
-
-echo "</table>";
+$arrayStr = ['first', 'second', 'third'];
+echo task1($arrayStr, true);
+?>
